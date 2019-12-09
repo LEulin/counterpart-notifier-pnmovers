f7af9c2132f54412c29e340365d14aeec49f7719
romeo-art <romeo.lenizo@student.passerellesnumeriques.org>
2019-12-09 10:55:28 +0800

    Finally Done

---
+++ index.js
@@ -0,0 +1,134 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var exp = require('express');
+var fs = require('fs');
+
+
+var port = process.env.PORT || 3000;
+var users = [];
+var actualuser = '';
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/notify.html');
+});
+
+app.use(exp.static('public'));
+const Lealyn = {
+    "id": 1,
+    "name": "Lealyn Eulin",
+    "Batch": "2020B",
+    "monthPaid": {
+        "January": true,
+        "February": true,
+        "March": true,
+        "April": true,
+        "May": true,
+        "June": true,
+        "July": true,
+        "August": true,
+        "September": true,
+        "October": true,
+        "November": true,
+        "December": false
+    }
+}
+const Faye = {
+    id: 1,
+    name: "Faye Erika Catalvas",
+    Batch: "2020B",
+    monthPaid: {
+        January: true,
+        February: true,
+        March: true,
+        April: true,
+        May: true,
+        June: true,
+        July: true,
+        August: true,
+        September: true,
+        October: true,
+        November: true,
+        December: false
+    }
+}
+var Jericho = {
+    id: 1,
+    name: "Jericho James Villahermosa",
+    Batch: "2020B",
+    monthPaid: {
+        January: true,
+        February: true,
+        March: true,
+        April: true,
+        May: true,
+        June: true,
+        July: true,
+        August: true,
+        September: true,
+        October: true,
+        November: true,
+        December: false
+    }
+}
+
+var users = [Lealyn,Faye,Jericho];
+var online_users = [];
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        users.forEach(element => {
+            if (element.username == msg.username) {
+                msg = { username: msg.username, msg: msg.msg, source: element.source }
+            }
+        });
+        io.emit('chat message', msg);
+    });
+
+    socket.on('broadcast', function(data) {
+        io.emit('broadcast', data)
+    })
+
+    socket.on('online', function(data) {
+        if (!online_users.includes(data.username)) {
+            online_users.push({username: data.username, text: "Paid"});
+        }
+        io.emit('online', online_users);
+    });
+
+    socket.on("typing", function(data) {
+        io.emit("typing", data);
+    });
+
+    socket.on("paid",async function(data) {
+        let user = data.username.toLowerCase()
+        if(user == "jericho"){
+            Jericho.monthPaid.December = true
+            io.emit("payment", {username:data.username, name:Jericho.name, month:"December"})
+        }else if(user == "faye"){
+            Faye.monthPaid.December = true
+            io.emit("payment", {username:data.username,name:Faye.name, month:"December"})
+        }else if(user == "lealyn"){
+            Lealyn.monthPaid.December = true
+            io.emit("payment", {username:data.username,name:Lealyn.name, month:"December"})
+        }
+        await online_users.forEach(element=>{
+            if(element.username == data.username){
+                element.text = "Done"
+            }
+        })
+        io.emit('online', online_users);
+    });
+
+    socket.once('disconnect', function() {
+        for (let i = 0; i < users.length; ++i) {
+            if (users[i].username == actualuser) {
+                users.splice(i, 1);
+            }
+        }
+        io.emit('logout', users)
+    })
+
+});
+
+http.listen(port, function() {
+    console.log('listening on *:' + port);
+});
---
+++ public/notify.js
@@ -0,0 +1,149 @@
+$(function () {
+    var username;
+    var admin = $('#admin');
+    // login select buttons
+    var adminLogin = $('#adminLogin');
+    var studentLogin = $('#studentLogin');
+    // select user login buttons
+    var adminBTN = $('#adminBTN');
+    var studentBTN = $('#studentBTN');
+    // for students
+    var notifier = $('#notifier');
+    var paidnotifier = $('#paidnotifier')
+    // login buttons to redirect to another actions
+    var studentbtnlogin = $('#studentbtnlogin');
+    var adminbtnlogin = $('#adminbtnlogin')
+    // for admin activity
+    var adminActivity = $('#adminActivity');
+    var url = "http://localhost:8080"
+    var paid = false;
+    var isStudent = false;
+
+    var okbtn = $('#oktbn');
+
+    $(adminLogin).hide();
+    $(studentLogin).hide();
+    $(adminActivity).hide();
+
+    $(studentBTN).click(function () {
+        $(adminLogin).hide();
+        $(studentLogin).show();
+    });
+
+    // functions for admin activity
+    $(adminBTN).click(function () {
+        $(studentLogin).hide();
+        $(adminLogin).show();
+        isStudent = false;
+    });
+    $(adminbtnlogin).click(function (e) {
+        Swal.fire(
+            'Successfully login!',
+            'Good job!',
+            'success'
+          )
+        e.preventDefault();
+        $(adminBTN).hide();
+        $(studentBTN).hide();
+        $(adminLogin).hide();
+        $(studentLogin).hide();
+        $(adminActivity).show();
+        isStudent = false;
+    });
+
+    // functions for the student notifier
+    $(studentbtnlogin).click(function (e) {
+        Swal.fire(
+            'Successfully login!',
+            'Good job!',
+            'success'
+          ) 
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            crossDomain: true,
+            url: `${url}/login`,
+            data: { username: $("#user").val() },
+            success: function (data) {
+                console.log(data);
+            }
+        })
+        $(adminBTN).hide();
+        $(studentBTN).hide();
+        $(adminLogin).hide();
+        $(studentLogin).hide();
+        $(notifier).show();
+        isStudent = true;
+    });
+
+    $("#okbtn").click(function (e) {
+        e.preventDefault();
+        $(adminBTN).hide();
+        $(studentBTN).hide();
+        $(adminLogin).hide();
+        $(studentLogin).hide();
+        $(notifier).hide();
+
+        window.setTimeout(function () {
+            $(notifier).show();
+        }, 3000)
+
+    });
+
+    var socket = io();
+    $('#studentbtnlogin').click(function (e) {
+        e.preventDefault();
+        username = $("#user").val()
+        socket.emit('online', { username: $("#user").val() })
+    })
+
+    var online = []
+    socket.on('online', function (data) {
+        $("#tbody").empty()
+        console.log("data: ", data)
+        data.forEach(element => {
+            console.log("enter..", data)
+            if (!online.includes(element.username)) {
+                newOnline(element.username, element.text)
+            }
+        });
+    })
+    $("#tbody").on('click', 'button', function (event) {
+        Swal.fire(
+            'Successfully added!',
+            'Good job!',
+            'success'
+          ) 
+        let user = $(this).attr('name');
+        let txt = $(this).text()
+        if (txt == "Paid") {
+            socket.emit('paid', { username: user })
+        } else {
+            Swal.fire(
+                'Already paid!',
+                'Nice one!',
+                'success'
+              ) 
+        }
+    });
+
+    function newOnline(username, text) {
+        let btn = "<button name='" + username + "'>" + text + "</button>"
+        let row = "<tr><th scope='row'>" + username + "</th><td>" + btn + "</td></tr>"
+        $("#tbody").append(row)
+    }
+
+    socket.on('payment', function (data) {
+        newPayment(data.name, data.month)
+        if (username == data.username) {
+            $(notifier).hide()
+            $("#paidnotifier").show()
+        }
+
+    })
+
+    function newPayment(name, month) {
+        let row = "<tr><td>" + name + "</td><td>" + month + "</td><td>500.00</td><td>Paid</td></tr>"
+        $("#paymentBody").append(row)
+    }
+});
